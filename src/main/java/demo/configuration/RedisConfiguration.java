f3a5023f0efdf3672c8c4f814afb1e659e5f57da
ravich <ravich@user-Latitude-3550>
2019-10-26 01:12:40 -0400

    added info

--- src/main/java/demo/configuration/RedisConfiguration.java
+++ src/main/java/demo/configuration/RedisConfiguration.java
@@ -1,10 +1,8 @@
 package demo.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;;
+import org.springframework.context.annotation.Profile;
 
-import demo.model.Person;
 
 @Configuration
 @Profile("redis")
